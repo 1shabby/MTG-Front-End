e9bd70066105b44298c6cd997a0972418dc51219
1shabby <67873454+1shabby@users.noreply.github.com>
2022-11-05 22:39:37 -0500

    Built up the Card Component
    Built up the card component a bit more. Likely will break the card details out into it's own component in the future.

--- mtg-front-end/src/Components/Card.js
+++ mtg-front-end/src/Components/Card.js
@@ -1,12 +1,23 @@
 import "./Card.css"
 
+
+//The image source will be pulled from the api request
+//Price and quantity will be pulled from the database
 export default function Card(){
     return(
         <div>
-            <img className="card--img" src="https://cards.scryfall.io/large/front/1/7/17d6703c-ad79-457b-a1b5-c2284e363085.jpg?1592673422"></img>
-            <div className="card--details">
-                <p>Price:</p>
-                <p>Quantity:</p>    
+            <div className="card--info">
+                <img className="card--img" src="https://backs.scryfall.io/large/4/0/407ed11d-7b6f-4e1c-ab00-f49c25f170e3.jpg?1665006169"></img>
+                    <div className="card--details">
+                        <p className="card--title">Card Name</p>
+                        <div className="card--sets">
+                            <p className="set--header">Sets</p>    
+                        </div>
+                    </div>
+                </div>
+                <div className="card--price_quantity">
+                <p>Price: Null</p>
+                <p>Quantity: Null</p>    
             </div>
         </div>
     )
