571295d1cb01f01c8565dc737cfea2caea2d6cde
1shabby <67873454+1shabby@users.noreply.github.com>
2022-11-25 17:46:59 -0600

    Removed an un-needed logo
    Removed an old logo, and Started working on the logic for flipping the card

--- mtg-front-end/src/Components/Card.js
+++ mtg-front-end/src/Components/Card.js
@@ -3,27 +3,32 @@ import CardDetails from "./CardDetails"
 import {useState} from 'react';
 import foil from "../Images/foil_overlay.png"
 
+
 export default function Card(props){
     const [isShown, setIsShown] = useState(false);
-
-    // const [front, setFlipped] = useState(true);
-
     const details = event =>{
         setIsShown(current => !current);
     };
-    // var image
-    // const flip = event =>{
-    //     setFlipped(image => {
-    //         if(front){
-    //             image = props.version[0].frontImage
-    //         }
-    //         else{
-    //             image = props.version[0].backImage
-    //         }
-    //         front = !front;
-    //     });
-    // }
+     
+    var [side,setSide] = useState(["front",props.version[0].frontImage]);
 
+    const flip = event =>{
+        // console.log("state " + side[0])
+        // console.log("Img " + side[1])
+        setSide(function(side){
+            if(side[0] == "front"){
+                side[0] = "back";
+                side[1] = props.version[0].backImage
+            }
+            else if(side[0] == "back"){
+                side[0] = "front";
+                side[1] = props.version[0].frontImage
+            }
+            console.log(side)
+            return side
+        })
+        
+    }
     return(
         <div>
             <div className="card--info">
@@ -33,14 +38,14 @@ export default function Card(props){
                     )}
                     <img
                         className='card--img'
-                        src={props.version[0].frontImage}></img>
+                        src={side[1]}></img>
             </div>
             {isShown && (
-                <CardDetails name={props.name} version={props.version}/>
+                <CardDetails name={side[0]} version={props.version}/>
             )}
             <div className="card--buttons">
                 <button className="card--button" onClick={details}>Details</button>
-                {props.version[0].backImage != "" && <button className="card--flip">Flip Card</button>}
+                {props.version[0].backImage != "" && <button className="card--flip" onClick={flip}>Flip Card</button>}
             </div>
             </div>
         </div>
