0ae1f76f2b6fcad0232bf9c03371913e057edf85
1shabby <67873454+1shabby@users.noreply.github.com>
2022-11-24 19:45:52 -0600

    Updated the Test Data Object Structure

--- mtg-front-end/src/TestData.js
+++ mtg-front-end/src/TestData.js
@@ -1,28 +1,48 @@
 export default [{
     name: "Adeliz, the Cinder Wind",
-    image: "https://cards.scryfall.io/normal/front/b/3/b3c950e2-a43b-47f8-9ad6-1909ccc7acbf.jpg?1562741514",
-    price: 0.19,
-    quantity: 1,
-    extra: "NONE",
-    collectorNumber: "",
-    sets: ["DOM"], 
+    version: [
+        {
+            image: "https://cards.scryfall.io/normal/front/b/3/b3c950e2-a43b-47f8-9ad6-1909ccc7acbf.jpg?1562741514",
+            price: 0.19,
+            quantity: 1,
+            extra: "NONE",
+            collectorsNumber: "",
+            set : "DOM"
+        }
+    ] 
 },
 {
     name: "Admiral Beckett Brass",
-    image: "https://cards.scryfall.io/normal/front/9/8/9854c2ec-0e53-4ac7-b417-554a9331c5e0.jpg?1562560523",
-    price: 1.28,
-    quantity: 1,
-    extra: "NONE",
-    collectorNumber: "",
-    sets: ["XLN"], 
+    version: [
+        {
+            image: "https://cards.scryfall.io/normal/front/9/8/9854c2ec-0e53-4ac7-b417-554a9331c5e0.jpg?1562560523",
+            price: 1.28,
+            quantity: 1,
+            extra: "NONE",
+            collectorsNumber: "",
+            set : "XLN"
+        }
+    ] 
 },
 {
     name: "Alela, Artful Provocateur",
-    image: "https://cards.scryfall.io/normal/front/7/2/726e7dc5-2089-4758-93e1-79212aedf75f.jpg?1591605150",
-    price: 0.97,
-    quantity: 1,
-    extra: "NONE",
-    collectorNumber: "",
-    sets: ["ELD"], 
+    version: [
+        {
+            image: "https://cards.scryfall.io/normal/front/7/2/726e7dc5-2089-4758-93e1-79212aedf75f.jpg?1591605150",
+            price: 0.97,
+            quantity: 1,
+            extra: "NONE",
+            collectorsNumber: "",
+            set : "ELD"
+        },
+        {
+            image: "https://cards.scryfall.io/large/front/4/3/43d28c05-0ba1-4afa-a784-b71741375507.jpg?1669101584",
+            price: 0.50,
+            quantity: 1,
+            extra: "NONE",
+            collectorsNumber: "",
+            set : "BRC"
+        }
+    ]
 }
 ]
